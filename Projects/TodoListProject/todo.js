84a96ec21c517a49cf7b69e15f03cb3a68b91242
semihkalkan <semihkalkan07@gmail.com>
2020-10-22 00:46:44 +0300

    first commit

---
+++ Projects/TodoListProject/todo.js
@@ -0,0 +1,29 @@
+//select to elements
+
+const form = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo");
+const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const filter = document.querySelector("#filter");
+const clearButton = document.querySelector("#clear-todos");
+
+eventListeners();
+
+function eventListeners(){  //all event listeners
+
+form.addEventListener("submit",addTodo);
+
+
+}
+
+function addTodo(e){
+
+ const newTodo = todoInput.value.trim() ;
+ 
+ console.log(newTodo);
+
+
+
+    e.preventDefault();
+}
